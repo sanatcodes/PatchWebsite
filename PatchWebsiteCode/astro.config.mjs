5008c02af6a4444714a3b793e2b03eb9f7f2d4ef
timf34 <farrelti@tcd.ie>
2026-01-12 19:57:55 +0000

    impact report redirect

--- PatchWebsiteCode/astro.config.mjs
+++ PatchWebsiteCode/astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    '/impact-report': 'https://joinpatch.s3.eu-west-1.amazonaws.com/Patch+Demo+Day+2025+Impact+Report.pdf'
+  }
+});
